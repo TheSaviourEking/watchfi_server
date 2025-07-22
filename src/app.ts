12f329d2e6a0d84643641b18b4b1c9722b931f5a
TheSaviourEking <saviour.eking@cyberservices.com>
2025-07-22 20:03:01 +0100

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const server = fastify({
 // Register CORS plugin
 await server.register(import('@fastify/cors'), {
     origin: process.env.NODE_ENV === 'production'
-        ? ['https://watchfi-server.onrender.com', 'https://watchfi-prod.onrender.com']
+        ? ['https://watchfi-server.onrender.com', 'https://watchfi-prod.onrender.com', "https://watchfi-client.vercel.app"]
         : true,
     credentials: true,
 });
